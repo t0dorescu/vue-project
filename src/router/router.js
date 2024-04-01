74e7cc3414f25a3d9c3d709a90c7d79d879dedbf
t0dorescu <todorescu.consulting@gmail.com>
2024-04-01 12:50:03 +0300

    #

--- src/router/router.js
+++ src/router/router.js
@@ -3,11 +3,19 @@ import { createMemoryHistory, createRouter } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
 import UserView from "@/views/UserView.vue";
+import UserGeneric from "@/views/UserGeneric.vue";
+import NotFound from "@/views/NotFound.vue";
 
 const routes = [
   { path: "/", component: HomeView },
   { path: "/about", component: AboutView },
-  { path: "/users/:id", component: UserView }
+  { path: "/users/:id", component: UserView },
+
+  // will match everything and put it under `route.params.pathMatch`
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
+
+  // will match anything starting with `/user-` and put it under `route.params.afterUser`
+  { path: "/user-:afterUser(.*)", component: UserGeneric }
 ];
 
 export const router = createRouter({
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,3 +1,19 @@
+<script setup>
+const route = useRoute();
+
+watch(
+  () => route.params.id,
+  (newId, oldId) => {
+    console.log({ newId, oldId });
+  }
+);
+
+onBeforeRouteUpdate(async (to, from) => {
+  console.log({ to, from });
+  // userData.value = await fetchUser(to.params.id)
+});
+</script>
+
 <template>
   <div>
     <!-- The current route is accessible as $route in the template -->
